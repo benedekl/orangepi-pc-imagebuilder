10288830c2f69d42dbea0edd3b6b56ceccaefe89
BENEDEK László <benedekl@gmail.com>
2019-05-12 22:02:22 +0200

    bugfix

--- image-builder/assets/boot.cmd
+++ image-builder/assets/boot.cmd
@@ -3,6 +3,6 @@ setenv fdt_high ffffffff
 setenv bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk0p2 rootwait panic=10
 
 fatload mmc 0 $kernel_addr_r zImage
-fatload mmc 0 $fdt_addr_r dtbs/sun8i-h3-orangepi-pc.dtb
+fatload mmc 0 $fdt_addr_r dtb/sun8i-h3-orangepi-pc.dtb
 
 bootz $kernel_addr_r - $fdt_addr_r
