adb491d4d9910e2728211e71757a590e0b76c6e1
BENEDEK László <benedekl@gmail.com>
2019-05-12 19:48:33 +0200

    move directories

--- image-builder/docker-entrypoint.sh
+++ image-builder/docker-entrypoint.sh
@@ -4,18 +4,18 @@ set -xue
 echo Hello,
 echo This is Debian Image Builder for the OrangePi PC.
 
-# enable foreign architecture in binfmts
-# needed for package installation in rootfs
+# Enable foreign architecture in binfmts
+# Needed for package installation in rootfs
 mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc
 update-binfmts --enable qemu-arm
 
-cd ${BUILD_ROOT}
+cd "${WORKDIR}"
 
 if [ $# -ne 0 ] ; then
     exec "$@"
 else
     #rm .uboot_configured
-    rm -f .boot*
-    rm -f boot.img boot.cmd bootp?.*
-    make -f /Makefile
+#    rm -f .boot*
+#    rm -f boot.img boot.cmd bootp?.*
+    make -f "${ASSETS}/Makefile"
 fi
